fed414e8eaf8b9af53a7e2be397b57858e8082cc
shruti166 <shruti.chemistry.lm@gmail.com>
2023-01-02 15:32:54 +0530

    added

--- chapter3/src/App.js
+++ chapter3/src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
+import Header from './components/Header/Header';
+import Board from './pages/Board';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header/>
+      <Board/>
     </div>
   );
 }
---
+++ chapter3/src/components/Header/Header.js
@@ -0,0 +1,11 @@
+
+
+function Header() {
+  return (
+    <div className="Header-wrapper">
+      <h1>Project Management Board</h1>
+    </div>
+  )
+}
+
+export default Header;
