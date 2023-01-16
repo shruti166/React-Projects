249047bb00cc072dc5c28c887d84f97baf3a7ac3
shruti166 <shruti.chemistry.lm@gmail.com>
2023-01-16 15:44:34 +0530

    added

--- mynewsbuzz/src/components/News.jsx
+++ mynewsbuzz/src/components/News.jsx
@@ -2,20 +2,37 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
+   
+    constructor() {
+        super();
+        this.state = {
+            articles : [],
+            loading : false
+        }
+    }
+    
+
+    async componentDidMount() {
+        let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=35cb7732fed34e719c13106477f35e8e";
+        let data = await fetch(url);
+        let parsedData = await data.json();
+       
+        this.setState({articles : parsedData.articles})
+       
+    }
   render() {
     return (
       <div className='container my-3'>
         <h2>NewsBuzz Top Headlines</h2>
+       
         <div className='row'>
-            <div className='col-md-4'>
-                <NewsItem title = "mytitle" description = "myDesc"/>
-            </div>
-            <div className='col-md-4'>
-                <NewsItem title = "mytitle" description = "myDesc"/>
-            </div>
-            <div className='col-md-4'>
-                <NewsItem title = "mytitle" description = "myDesc"/>
-            </div>
+            {this.state.articles.map((element) => {
+              return <div className='col-md-4' key = {element.url}>
+              <NewsItem title = {element.title ? element.title : ""} description = {element.description ? element.description : ""} urlToImage = {!element.urlToImage ?  "https://thumbs.dreamstime.com/b/news-header-background-title-abstract-colorful-global-map-text-hightech-design-blue-colorful-template-90494676.jpg" : element.urlToImage}/>
+          </div>  
+            })}
+            
+           
             
            
         </div>
--- mynewsbuzz/src/components/NewsItem.jsx
+++ mynewsbuzz/src/components/NewsItem.jsx
@@ -2,15 +2,15 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
-    let {title, description} = this.props
+    let {title, description, urlToImage} = this.props
     return (
     
     <div className="card" style={{width: "18rem"}}>
-        <img src="..." class="card-img-top" alt="..."/>
+        <img src={urlToImage}className="card-img-top" alt="..."/>
         <div className="card-body">
-            <h5 class="card-title">{title}</h5>
+            <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
-            <a href="/" class="btn btn-primary">View more</a>
+            <a href="/" className="btn btn-primary">View more</a>
         </div>
     </div>
     
