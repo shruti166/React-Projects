bd0eceb4edf0e277c61c8f98252af1c404ee88f8
shruti166 <shruti.chemistry.lm@gmail.com>
2023-01-17 11:34:35 +0530

    routes updated

--- mynewsbuzz/src/App.js
+++ mynewsbuzz/src/App.js
@@ -1,16 +1,33 @@
-
 import './App.css';
-import Navbar from './components/Navbar';
+
+import React, { Component } from 'react'
+import NavBar from './components/Navbar';
 import News from './components/News';
+import {
+  BrowserRouter as Router,
+  
+  Route,
+  Routes,
+} from "react-router-dom";
 
+export default class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-     <Navbar/>
-     <News pageSize = {5} country = "in" />
-    </div>
-  );
+  render() {
+    return (
+      <div>
+        <Router>
+        <NavBar/> 
+        <Routes>
+          <Route exact path="/" element = {<News key="general" pageSize={5} country="in" category="general"/> }/>
+          <Route exact path="/entertainment" element = {<News key="entertainment" pageSize={5} country="in" category="entertainment"/> }/>
+          <Route exact path="/business" element = {<News key="business" pageSize={5} country="in" category="business"/>} />
+          <Route exact path="/science" element = {<News key="science" pageSize={5} country="in" category="science"/>} /> 
+          <Route exact path="/technology" element = {<News key="technology" pageSize={5} country="in" category="technology"/>} />
+          <Route exact path="/sports" element = {<News key="sports" pageSize={5} country="in" category="sports"/>} />
+          
+        </Routes>
+        </Router>
+      </div>
+    )
+  }
 }
-
-export default App;
--- mynewsbuzz/src/components/News.jsx
+++ mynewsbuzz/src/components/News.jsx
@@ -29,7 +29,7 @@ export class News extends Component {
     
 
     async componentDidMount() {
-        let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=35cb7732fed34e719c13106477f35e8e&page=1&pageize=20";
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=dbe57b028aeb41e285a226a94865f7a7&page=1&pageSize=${this.props.pageSize}`
         let data = await fetch(url);
         let parsedData = await data.json();
        
