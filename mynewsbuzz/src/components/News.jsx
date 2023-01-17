1a54863b5512265f3193c58995b67fda0bc29644
shruti166 <shruti.chemistry.lm@gmail.com>
2023-01-17 15:20:38 +0530

    infinitescroll added

--- mynewsbuzz/src/components/News.jsx
+++ mynewsbuzz/src/components/News.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
+import InfiniteScroll from "react-infinite-scroll-component";
+
 
 export class News extends Component {
 
@@ -22,7 +24,8 @@ export class News extends Component {
             articles : [],
             loading : false,
             page : 1,
-            totalResults: ""
+            totalResults: "",
+            
         }
     }
     
@@ -57,33 +60,44 @@ export class News extends Component {
             this.setState({articles : parsedData.articles, page : this.state.page+1})
         }
     }
+    fetchMoreData = async () => {  
+      this.setState({page: this.state.page + 1})
+      const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=d093053d72bc40248998159804e0e67d&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+      let data = await fetch(url);
+      let parsedData = await data.json()
+      this.setState({
+          articles: this.state.articles.concat(parsedData.articles),
+          totalResults: parsedData.totalResults
+      })
+    };
     
   render() {
     return (
-      <div className='container my-3'>
-        <h2>NewsBuzz Top Headlines</h2>
-       
-        <div className='row'>
-            {this.state.articles.map((element) => {
-              return <div className='col-md-4' key = {element.url}>
-              <NewsItem title = {element.title ? element.title : ""} description = {element.description ? element.description : ""} urlToImage = {!element.urlToImage ?  "https://thumbs.dreamstime.com/b/news-header-background-title-abstract-colorful-global-map-text-hightech-design-blue-colorful-template-90494676.jpg" : element.urlToImage}/>
-          </div>  
-            })}
-            <div className='container d-flex justify-content-between'>
-                <button disabled = {this.state.page <= 1} type="button" className="btn btn-secondary btn-lg" onClick={this.handlePrev}>Prev</button>
-                <button disabled = {this.state.page + 1 > Math.ceil(this.state.totalResults/20)} type="button" className="btn btn-secondary btn-lg" onClick={this.handleNext}>Next</button>
-            </div>
-            
-           
-            
-           
-        </div>
-       
-        
-      </div>
-    )
-  }
-}
+      <>
+                <h1 className="text-center" style={{ margin: '35px 0px' }}> NewsBuzz - Top {this.props.category} Headlines </h1>
+                
+                <InfiniteScroll
+                    dataLength={this.state.articles.length}
+                    next={this.fetchMoreData}
+                    hasMore={this.state.articles.length !== this.state.totalResults}
+                    
+                > 
+                    <div className="container">
+                         
+                    <div className="row">
+                        {this.state.articles.map((element) => {
+                            return <div className="col-md-4" key={element.url}>
+                                <NewsItem title={element.title ? element.title : ""} description={element.description ? element.description : ""} urlToImage={element.urlToImage} newsUrl={element.url}   />
+                            </div>
+                        })}
+                    </div>
+                    </div> 
+                </InfiniteScroll>
+
+            </>
+           )
+          }
+      } 
 
 export default News
 
