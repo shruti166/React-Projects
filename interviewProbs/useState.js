4c32e2e74cf1b3b8cfa17f87dc3693e93c9f0216
shruti166 <shruti.chemistry.lm@gmail.com>
2023-02-20 18:53:40 +0530

    added

--- interviewProbs/useState.js
+++ interviewProbs/useState.js
@@ -1,6 +1,39 @@
+//Q1 Write a code to Handle multiple input tags with a single function //
+
 const [counter, setCounter] = useState("");
 const [moreStuff, setMoreStuff] = useState("");
 
 const setCount = () => {
     setCounter(counter+1);
+}
+
+//Q1 Write a code to Handle multiple input tags with a single function //
+
+const initState = {
+    name: "",
+    email: "",
+    password: ""
+}
+
+function App()  {
+    const [state, setState] = React.useState(initState);
+
+    const handleChange = e => {
+        const {name, value} = e.target;
+        setState({...state, [name]: value})
+    }
+    return (
+        <div>
+            <div>
+                <input type = "text" placeholder = "name" value = {state.name} onChange = {handleChange}/>
+            </div>
+            <div>
+                <input type = "text" placeholder="email" value = {state.email} onChange = {handleChange}/>
+            </div>
+            <div>
+                <input type = "password" placeholder = "password" value = {state.password} onChange = {handleChange}/>
+            </div>
+            
+        </div>
+    )
 }
