77b800754a4c0320c68e6b24323abfc6781557a1
shruti166 <shruti.chemistry.lm@gmail.com>
2023-02-27 20:34:12 +0530

    added

--- myreduxform/src/components/NewUser.js
+++ myreduxform/src/components/NewUser.js
@@ -1,17 +1,26 @@
 import React, {useState} from "react";
 import { Action } from "../Action/action";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 
-export default function NewUser() {
+
+export default function NewUser(props) {
     const [userData, setUserData] = useState('Shruti');
     const dispatch = useDispatch();
     const sendData = () => {
         Action(userData, dispatch);
 
     }
+    const data = useSelector((storeData) => {
+        return storeData.users;
+        
+    })
+   
   return <div>
     <input placeholder="Enter" value = {userData} onChange ={(e)=> {setUserData(e.target.value)}}/>
     <button onClick={sendData}>ADD USER</button>
+    {data.map((elem) => {
+        return <h2>{elem}</h2>
+    })}
   </div>;
 }
