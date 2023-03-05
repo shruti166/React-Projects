f147b056e9a23fe69c93fa9f677e1da39d7a7c62
shruti166 <shruti.chemistry.lm@gmail.com>
2023-03-06 00:01:05 +0530

    added

--- react/index.js
+++ react/index.js
@@ -56,3 +56,16 @@ class Parent extends React.Component {
             )
         }
     }
+
+
+let data = [1, 2, 3, 4, 5];
+
+Array.prototype.myMap = function (cb) {
+  let arr = [];
+  for (let i = 0; i < this.length; i++) {
+    arr.push(cb(this[i], i, this));
+  }
+  return arr;
+};
+const mapLog = data.myMap((el) => el * 2);
+console.log(mapLog);
